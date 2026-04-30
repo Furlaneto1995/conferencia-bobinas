452ee744ad6235f95c77c3579d390bdfb17e38c5
Furlaneto1995 <54684464+Furlaneto1995@users.noreply.github.com>
2026-04-29 23:44:25 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'bobinas-v1';
+const FILES = [
+  '/conferencia-bobinas/',
+  '/conferencia-bobinas/index.html',
+  '/conferencia-bobinas/camera.html',
+  '/conferencia-bobinas/manifest.json',
+  'https://unpkg.com/html5-qrcode@2.3.8/html5-qrcode.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
